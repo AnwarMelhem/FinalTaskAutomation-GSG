c30becc97c45e00b48519b561b590d7ccffedf65
AnwarMelhem <anwarmelhem2000@gmail.com>
2023-11-09 22:27:49 +0200

    Function to clicks to "BIM" tab

--- cypress/support/pageObjects/dashoboard.ts
+++ cypress/support/pageObjects/dashoboard.ts
@@ -1,10 +1,10 @@
 // This POM to dashboard section
 export default class Dashboard {
-// Function to get main menue items
+  // Function to get main menue items
   static mainMenuItems() {
     return cy.get(".oxd-sidepanel-body");
   }
-// Sunction to clicks to "BIM" tab
+  // Function to clicks to "BIM" tab
   static clicksToBIMtab() {
     cy.intercept("/web/index.php/api/v2/pim/employees**").as("employees");
     cy.intercept("/web/index.php/api/v2/admin/job-titles**").as("jobTitles");
@@ -23,5 +23,15 @@ export default class Dashboard {
       "@subunits",
       "@messages",
     ]);
+  }
+  // Function to clicks to "BIM" tab
+  static clicksToClaimTab() {
+    cy.intercept("/web/index.php/api/v2/claim/**").as("claim");
+    cy.intercept("/web/index.php/api/v2/leave/workweek**").as("leave");
+    cy.intercept("/web/index.php/api/v2/leave/holidays**").as("holidays");
+
+    this.mainMenuItems().contains("Claim").click({ force: true });
+
+    cy.wait(["@claim", "@leave", "@holidays"]);
   }
 }
